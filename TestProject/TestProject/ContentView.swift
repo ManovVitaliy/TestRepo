f93f58aba4b9e1581f52c809a1402abce0403ea7
Vitaliy Manov <vitaliy.manov@chisw.com>
2020-04-02 12:43:49 +0300

    Initial commit

---
+++ TestProject/TestProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestProject
+//
+//  Created by Illia Khrypunov on 4/2/20.
+//  Copyright © 2020 Illia Khrypunov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
